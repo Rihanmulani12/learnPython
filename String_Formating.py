8587568a3735978ed8057ed73932fba22f61e9aa
Rihanmulani12 <rihanmulani18@gmail.com>
2024-02-03 13:36:02 +0530

    Merge remote-tracking branch 'main/main'

---
+++ String_Formating.py
@@ -0,0 +1,22 @@
+name = "rohan"
+print("hello %s" %name)
+
+num = 12
+
+print("this is %d" %num)
+
+
+print("hello my name is %s and my role num is %d" %(name, num))
+
+
+print("for num is %d ,  for str %s , and for float is %s")
+
+
+str = "Rihan Mulani"
+print(len(str))
+
+
+print(str.index("i"))
+print(str.index("u"))
+
+print(str.count("i"))
