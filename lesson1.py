20c0821e88300dd2e27a6569bbfa888e87aa1ba5
Rihanmulani12 <rihanmulani18@gmail.com>
2024-02-03 12:02:01 +0530

    leran python

--- lesson1.py
+++ lesson1.py
@@ -1,2 +1,21 @@
 
+# print("Rihan")
+#
+# print("rihan kashim mulani")
+#
+# print("hello world")
+#
+# print(12)
 
+x = 12
+if x==4:
+    print("yess")
+else:
+    print("no")
+
+
+y = 10
+if y==12:
+    print("yes")
+else:
+    print("no")
