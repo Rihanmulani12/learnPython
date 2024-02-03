fc92bbb43b424bac647e054abee267a004edaf9c
Rihanmulani12 <rihanmulani18@gmail.com>
2024-02-03 12:15:57 +0530

    leran python

--- lesson1.py
+++ lesson1.py
@@ -1,21 +1,50 @@
-
-# print("Rihan")
+# #
+# # # print("Rihan")
+# # #
+# # # print("rihan kashim mulani")
+# # #
+# # # print("hello world")
+# # #
+# # # print(12)
+# #
+# # # x = 12
+# #
+# # # if x==4:
+# # #     print("yess")
+# # # else:
+# # #     print("no")
+# #
+# # #
+# # # y = 10
+# # # if y==12:
+# # #     print("yes")
+# # # else:
+# # #     print("no")
+# #
+# #
+# # mynum = 7
+# # print(mynum)
+# #
+# # myfloat = 7.4
+# # print(myfloat)
+# #
+# # mynum = float(mynum)
+# #
+# # newnum = float(10)
+# # print(mynum)
+# # print(newnum)
 #
-# print("rihan kashim mulani")
 #
-# print("hello world")
+# one = 1
+# two = 2
+# three = one + two
+# print(three)
 #
-# print(12)
-
-x = 12
-if x==4:
-    print("yess")
-else:
-    print("no")
-
-
-y = 10
-if y==12:
-    print("yes")
-else:
-    print("no")
+# hello = "hello"
+# world = "world"
+#
+# helloworld = hello + " "+world
+# print(helloworld)
+#
+# rihanmulani = "Rihan"+ " " + "Mulani"
+# print(rihanmulani)
