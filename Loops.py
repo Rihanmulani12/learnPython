f802b1b4bd57778757ef48ed8af629f35e2510af
Rihanmulani12 <rihanmulani18@gmail.com>
2024-02-04 13:30:21 +0530

    Merge remote-tracking branch 'main/main'

---
+++ Loops.py
@@ -0,0 +1,63 @@
+# prime = [2, 3, 5, 7]
+# # for i in prime:
+# #     print(i)
+#
+# myarr = [1,2,3,4,5,6]
+# for i in myarr:
+#     if i % 2==0:
+#         print(i)
+#
+# for x in range(1,5):
+#     print(x)
+#
+# myarr = []
+# for y in range(1,21):
+#     myarr.append(y)
+#
+# print(myarr)
+# myarr.pop(2)
+#
+#
+# for x in range(10):
+#     print( not (x % 2==0))
+#
+#
+# count = 0
+# while count < 5:
+#     count += 1
+#     print(count)
+
+
+# i = 10
+#
+# while i<15:
+#     i+=1
+#     print(i)
+
+
+#breake and countine
+
+# count = 0
+# while count < 10:
+#     count+=1
+#     print(count)
+#
+#     # if count== 5:
+#     #     break
+#
+#
+#
+#     if count == 5:
+#         break
+
+
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+
+for x in range(0,10):
+    if x==5:
+        continue
+    print(x)
