3fd6708f369d121928e43467d8ba7ab19666f95a
Rihanmulani12 <rihanmulani18@gmail.com>
2024-02-04 13:50:54 +0530

    Merge remote-tracking branch 'main/main'

---
+++ Class_and_Objects.py
@@ -0,0 +1,19 @@
+class myclass:
+    variable = 12
+
+    def myfun(self):
+        print("hello wolrd")
+
+
+myclass.variable = 17
+print(myclass.variable)
+
+
+class myschhole:
+    name = "MyHighSchol"
+
+    def change_name(self):
+       myschhole.name = "Nagaon HighSchool"
+       return
+
+
